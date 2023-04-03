2f9d4c37c96ba3246a8273bfa2a0f38ed243de09
Maxime <m.kwizera@alustudent.com>
2023-04-03 14:19:52 +0200

    experience_rating

---
+++ experience_rating.py
@@ -0,0 +1,4 @@
+print('After using our application, How would you rate the experience?,Rate from 1 to 10, 10 being you had the best experience')
+rating = input('Rating: ')
+print(
+    F'You gave us a {rating} out of 10 rating, We will keep the idea in mind.')
